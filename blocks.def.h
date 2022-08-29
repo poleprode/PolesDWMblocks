e9a73c687b86bba911d870631020a62abd119fc7
poleprode <poleprode@github.com>
2022-08-30 02:06:48 +0300

    Added bluetooth and ssh icons

--- blocks.def.h
+++ blocks.def.h
@@ -1,11 +1,13 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/			/*Update Interval*/ /*Update Signal*/
-	{"",	"$XDG_CONFIG_HOME/dwmblocks/scripts/music",	10,	5},
-	{"",	"$XDG_CONFIG_HOME/dwmblocks/scripts/volume",	0,	4},
-	{"",	"$XDG_CONFIG_HOME/dwmblocks/scripts/cpu",	5,	3},
-	{"",	"$XDG_CONFIG_HOME/dwmblocks/scripts/ram",	10,	2},
-	{"",	"$XDG_CONFIG_HOME/dwmblocks/scripts/clock",	60,	1},	
+	{"",	"$HOME/.config/dwmblocks/scripts/ssh",		0,	7},
+	{"",	"$HOME/.config/dwmblocks/scripts/bluetooth",	0,	6},
+	{"",	"$HOME/.config/dwmblocks/scripts/music",	10,	5},
+	{"",	"$HOME/.config/dwmblocks/scripts/volume",	0,	4},
+	{"",	"$HOME/.config/dwmblocks/scripts/cpu",		5,	3},
+	{"",	"$HOME/.config/dwmblocks/scripts/ram",		10,	2},
+	{"",	"$HOME/.config/dwmblocks/scripts/clock",	60,	1},	
 };
 
 //Sets delimiter between status commands. NULL character ('\0') means no delimiter.
